61fbb155ca177bdd4cf882ec3f86abf8f8d0bdcb
Richard Cao <44596884+Richard-Cao226@users.noreply.github.com>
2021-02-05 18:38:31 -0800

    added two files for extracting frames from video

---
+++ extract_frame_every_few_seconds.py
@@ -0,0 +1,11 @@
+import cv2
+
+count = 0
+vidcap = cv2.VideoCapture('Test_Video_1280x720_5_sec.mp4')
+success,image = vidcap.read()
+while success:
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))      # saves frame every 1000 milliseconds
+    success,image = vidcap.read()
+    print ('Read a new frame: ', success)
+    cv2.imwrite( "Frames/frame%d.jpg" % count, image)     # save frame as JPEG file
+    count = count + 1

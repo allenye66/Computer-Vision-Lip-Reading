5ddcb39327c413c4f1f20c4a579ea00c9d37c0fa
Richard Cao <44596884+Richard-Cao226@users.noreply.github.com>
2021-02-06 15:04:33 -0800

    generated csv from dictionary with words and associated phonemes

---
+++ generate_csv.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('phonemes.csv', 'w', newline='') as file:
+
+	writer = csv.writer(file)
+	writer.writerow(["Word", "Phonemes"])
+
+	with open('phoneme_dictionary.txt') as fp:
+		line = fp.readline()
+		cnt = 1
+		while line:
+			word = ""
+			phonemes = ""
+			wordAdded = False
+			
+			for i in line.split():
+				if not wordAdded:
+					word = i
+					wordAdded = True
+				else:
+					phonemes = phonemes + i + " "
+			phonemes = phonemes[:-1]
+
+			writer.writerow([word, phonemes])
+			
+			cnt += 1
+			word = ""
+			phonemes = ""
+			line = fp.readline()
+
+			print("Line {}: {}".format(cnt, line.strip()))

a960a0bdb251ab2de670153870f3b866b8782a01
Richard Cao <44596884+Richard-Cao226@users.noreply.github.com>
2021-02-06 22:16:49 -0800

    created function to convert phonemes to word

---
+++ phoneme_to_word.py
@@ -0,0 +1,13 @@
+import csv
+import sys
+
+def phoneme_to_word(phonemes):
+	with open('phonemes.csv') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		line_count = 0
+		for row in csv_reader:
+			if (row[1] == phonemes):
+				return row[0]
+
+if __name__=="__main__":
+	print(phoneme_to_word(sys.argv[1]))

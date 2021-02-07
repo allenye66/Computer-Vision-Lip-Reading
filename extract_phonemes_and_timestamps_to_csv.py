13846603f1b7163e588f990081c40f87a1c4ecb3
Allen <allenye66@gmail.com>
2021-02-06 16:49:06 -0800

    clean the csv gentle gives by removing empty rows and bad data

--- extract_phonemes_and_timestamps_to_csv.py
+++ extract_phonemes_and_timestamps_to_csv.py
@@ -1,6 +1,44 @@
-inputfile = open('/Users/allen/Desktop/Automated-Speech-Recognition/align_example.txt')
 
-for i in range(4): inputfile.next() # skip first four lines
-for line in inputfile:
-    print(line)
+# use better method for now
+import csv
+input_text = open('/Users/allen/Desktop/Automated-Speech-Recognition/align_example.txt')
 
+
+aligned_word_arr = []
+word_arr = []
+
+fail_words = 0
+
+
+
+#the text file contains words that couldn't be transcriped, for example: 
+'''
+{
+      "case": "not-found-in-audio",
+      "endOffset": 1587,
+      "startOffset": 1582,
+      "word": "tried"
+},
+'''
+
+
+#first remove all these instances
+for line in input_text:
+	if '"case": "not-found-in-audio",' in line:
+		fail_words+=1
+print(fail_words)
+
+
+line_num = 0
+for line in input_text:
+	print(line)
+	line_num += 1
+	if '"alignedWord":' in line:
+		aligned_word_arr.append(line_num)
+	if '"word":' in line:
+		word_arr.append(line_num)
+
+print(len(aligned_word_arr))
+print(len(word_arr))
+#should equal zero and have 1:1 ratio
+print(len(aligned_word_arr)-len(word_arr))

e2216ab0a37531aac2f057c7830a458cd9e14a96
Richard Cao <44596884+Richard-Cao226@users.noreply.github.com>
2021-02-08 23:26:33 -0800

    created function to generate csv file with timestamps for phonemes

---
+++ generate_phoneme_timestamps.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('phoneme_timestamps.csv', 'w', newline='') as file:
+	writer = csv.writer(file)
+	writer.writerow(["Phoneme", "Start Time", "End Time"])
+
+	with open('align_example.txt') as fp:
+		line = fp.readline()
+		line_num = 0
+		duration_arr = []
+		phoneme_arr = []
+		while line:
+			if line_num > 2:
+				if 'duration' in line:
+					duration = line[line.index(':')+2:line.index(',')]
+					duration_arr.append(round(float(duration), 2))
+					line = fp.readline()
+					line_num = line_num + 1
+					phoneme = line[line.index(':')+3:line.index('_')]
+					phoneme_arr.append(phoneme)
+				if 'start\"' in line:
+					start_time = round(float(line[line.index(':')+2:line.index(',')]), 2)
+					while phoneme_arr:
+						end_time = round(start_time + duration_arr.pop(0), 2)
+						writer.writerow([phoneme_arr.pop(0), start_time, end_time])
+						start_time = end_time
+
+			
+			line = fp.readline()
+			line_num = line_num + 1
+

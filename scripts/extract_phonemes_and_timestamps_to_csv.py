98f89eb9c480f72619ea257a6fe380447bfc6c3c
Allen <allenye66@gmail.com>
2021-02-10 00:47:07 -0800

    added code to create csv file

--- scripts/extract_phonemes_and_timestamps_to_csv.py
+++ scripts/extract_phonemes_and_timestamps_to_csv.py
@@ -12,6 +12,12 @@
 #THE LENGTH OF THE CLEAN CSV HAS TO MATCH THE LENGTH OF "ALIGNED_WORD_ARR"
 
 
+'''
+STEPS:
+1. get gentle txt file
+2. remove the "case not found" and "<unk>"
+3. run it throw the code
+'''
 import csv
 input_text = open('align_example.txt')
 aligned_word_arr = []
@@ -52,51 +58,56 @@ text = [x.strip() for x in text]
 
 
 current_time = 0
-for i in range(len(aligned_word_arr)):
-	#print(aligned_word_arr[i]-1, word_arr[i]) # has the -1 because we need to included "alignedword"
-	current_word_info = []
-	for j in range(aligned_word_arr[i]-1, word_arr[i]):
-		current_word_info.append(text[j])
-	#print(current_word_info)
-	phonemes = []
-	start_times = []
-	end_times = []
-	durations = []
-	for k in current_word_info:
-		if "phones" not in k:
-			if "phone" in k:
-				phonemes.append(k[10:len(k)-3])
-		if "startOffset" not in k:
-			if "start" in k:
-				start_times.append(float(k[9:len(k)-1]))
 
-		
-		if "duration" in k:
-			durations.append(float(k[12:len(k)-1]))
-		#add to the end
-		#if "endOffset" not in i:
-	#		if "end" in i:
-	#			end_times.append(float(i[7:len(i)-1]))
+header = ['phoneme', 'start', 'end']
 
-	for k in range(len(durations)-1):
-		start_times.append(round(start_times[k-1]+durations[k], 2))
+with open('phonemes_start_end.csv', 'w') as g:
+	writer = csv.writer(g)
+	writer.writerow(header)
+	for i in range(len(aligned_word_arr)):
+		#print(aligned_word_arr[i]-1, word_arr[i]) # has the -1 because we need to included "alignedword"
+		current_word_info = []
+		for j in range(aligned_word_arr[i]-1, word_arr[i]):
+			current_word_info.append(text[j])
+		#print(current_word_info)
+		phonemes = []
+		start_times = []
+		end_times = []
+		durations = []
+		for k in current_word_info:
+			if "phones" not in k:
+				if "phone" in k:
+					phonemes.append(k[10:len(k)-3])
+			if "startOffset" not in k:
+				if "start" in k:
+					start_times.append(float(k[9:len(k)-1]))
 
-	end_times = start_times[1:]
-	end_times.append(round(start_times[-1]+durations[-1],2))
-	print(phonemes)
-	print(start_times)
-	print(end_times)
-	#print(durations)
+			
+			if "duration" in k:
+				durations.append(float(k[12:len(k)-1]))
+			#add to the end
+			#if "endOffset" not in i:
+		#		if "end" in i:
+		#			end_times.append(float(i[7:len(i)-1]))
 
+		for k in range(len(durations)-1):
+			start_times.append(round(start_times[k-1]+durations[k], 2))
 
-#	print(current_word_info)
-#	for i in current_word_info:
-#		print(i)	
-
-
-	#start_time =
-	#end_time = 
-	#csv save row
+		end_times = start_times[1:]
+		end_times.append(round(start_times[-1]+durations[-1],2))
+		print(phonemes)
+		print(start_times)
+		print(end_times)
+		#print(durations)
 
 
+	#	print(current_word_info)
+	#	for i in current_word_info:
+	#		print(i)	
+
+
+		#csv new row
+		for j in range(len(phonemes)):
+			row = [phonemes[j], start_times[j], end_times[j]]
+			writer.writerow(row)
 

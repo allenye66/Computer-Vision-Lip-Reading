66c23d961768f2bccbe7d76580b64a331315387a
Allen <allenye66@gmail.com>
2021-02-16 20:37:53 -0800

    example model

---
+++ cnn_results/results (8)/code.py
@@ -0,0 +1,17 @@
+model = Sequential()
+model.add(Conv1D(filters= 64, kernel_size=3, activation ='relu',strides = 2, padding = 'valid', input_shape= (19200, 1)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Conv1D(filters= 128, kernel_size=3, activation ='relu',strides = 2, padding = 'valid'))
+model.add(MaxPooling1D(pool_size=2))
+
+model.add(Dropout(0.9))
+model.add(MaxPooling1D(pool_size=2))
+
+model.add(Flatten())
+model.add(Dense(39)) 
+model.add(Activation('softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(X_train,y_train, epochs = 30, batch_size = 256, validation_data = (X_test, y_test), shuffle = True)
